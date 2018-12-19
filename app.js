a2e82b376b53cdfbda6d1abfc3d80bdfaf7d1353
Electric_Sheep <iamthebou@gmail.com>
2018-12-19 18:25:39 +0100

    Preparing the back for uploading files

--- app.js
+++ app.js
@@ -36,21 +36,21 @@ app.post("/api/cabinet", cabinetControllers.create);
 app.get("/api/cabinet", cabinetControllers.list);
 app.put("/api/cabinet/:cabinetId", cabinetControllers.update);
 app.delete("/api/cabinet/:cabinetId", cabinetControllers.destroy);
-app.post(
-  "/dashboard/moncompte",
-  upload.array("signature", 2),
-  (req, res, next) => {
-    console.log(req.files);
-    for (f of req.files)
-      fs.rename(f.path, "public/images/" + f.originalname, function(err) {
-        if (err) {
-          res.send("problème durant le déplacement");
-        } else {
-          res.send("Fichier uploadé avec succès");
-        }
-      });
-  }
-);
+// app.post(
+//   "/dashboard/moncompte",
+//   upload.array("signature", 2),
+//   (req, res, next) => {
+//     console.log(req.files);
+//     for (f of req.files)
+//       fs.rename(f.path, "public/images/" + f.originalname, function(err) {
+//         if (err) {
+//           res.send("problème durant le déplacement");
+//         } else {
+//           res.send("Fichier uploadé avec succès");
+//         }
+//       });
+//   }
+// );
 
 // CRUD routes for Creanciers
 app.post("/api/creanciers", creanciersController.create);
